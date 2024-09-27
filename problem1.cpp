7152a9571cdedadfe87ab0395d3dcacf5a8ed375
scapemm <a.shukurullayev@newuu.uz>
2024-09-27 12:55:57 +0500

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -9,7 +9,7 @@ int main() {
         for(int j=1; j<=k; j++){
             cout << j;
         }
-        for(int s=0; s<cnt; s++) cout <<' ';
+        cout <<' ';
         for(int j=k; j>0; j--)cout << j;
         cout << "\n";
         k--;
