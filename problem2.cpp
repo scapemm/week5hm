cdae3051cada67f6acd5d34d57e1004ae452247e
scapemm <a.shukurullayev@newuu.uz>
2024-09-27 13:06:07 +0500

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -3,16 +3,21 @@ using namespace std;
 int main() {
     int tt;
     cin >> tt;
+    vector<string> ans;
     while(tt--){
+        string s="";
         int n;
         cin >> n;
         string a = to_string(n);
         reverse(a.begin(),a.end());
         for(int i=0; i<a.size(); ++i){
-            cout << a[i];
-            if(i!=a.size()-1) cout << ' ';
+            s+= a[i];
+            if(i!=a.size()-1) s+= ' ';
         }
-        cout << "\n";
+        ans.push_back(s);
+    }
+    for(int i=0; i<ans.size(); ++i){
+        cout << ans[i] << "\n";
     }
     return 0;
 }
